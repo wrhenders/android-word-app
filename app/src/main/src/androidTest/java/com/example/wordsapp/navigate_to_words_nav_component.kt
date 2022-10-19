e27ee7b8227e16a7fbeb806f2e0e4c99e8b0427f
Ryan Henderson <wrhenders@gmail.com>
2022-10-19 14:30:01 -0500

    added testing for navigation and startup

---
+++ app/src/main/src/androidTest/java/com/example/wordsapp/navigate_to_words_nav_component.kt
@@ -0,0 +1,83 @@
+import androidx.test.espresso.DataInteraction
+import androidx.test.espresso.ViewInteraction
+import androidx.test.filters.LargeTest
+import androidx.test.ext.junit.rules.ActivityScenarioRule
+import androidx.test.ext.junit.runners.AndroidJUnit4
+import android.view.View
+import android.view.ViewGroup
+import android.view.ViewParent
+
+import androidx.test.InstrumentationRegistry.getInstrumentation
+import androidx.test.espresso.Espresso.onData
+import androidx.test.espresso.Espresso.onView
+import androidx.test.espresso.Espresso.pressBack
+import androidx.test.espresso.Espresso.openActionBarOverflowOrOptionsMenu
+import androidx.test.espresso.contrib.RecyclerViewActions.actionOnItemAtPosition
+import androidx.recyclerview.widget.RecyclerView.ViewHolder
+import androidx.test.espresso.action.ViewActions.*
+import androidx.test.espresso.assertion.ViewAssertions.*
+import androidx.test.espresso.matcher.ViewMatchers.*
+
+import com.example.wordsapp.R
+
+import org.hamcrest.Description
+import org.hamcrest.Matcher
+import org.hamcrest.TypeSafeMatcher
+import org.hamcrest.core.IsInstanceOf
+import org.junit.Rule
+import org.junit.Test
+import org.junit.runner.RunWith
+
+import org.hamcrest.Matchers.allOf
+import org.hamcrest.Matchers.anything
+import org.hamcrest.Matchers.`is`
+
+@LargeTest
+@RunWith(AndroidJUnit4::class)
+class navigate_to_words_nav_component {
+
+    @Rule
+    @JvmField
+    var mActivityScenarioRule = ActivityScenarioRule(MainActivity::class.java)
+
+    @Test
+    fun navigate_to_words_nav_component() {
+        val recyclerView = onView(
+            allOf(
+                withId(R.id.recycler_view),
+                childAtPosition(
+                    withClassName(`is`("android.widget.FrameLayout")),
+                    0
+                )
+            )
+        )
+        recyclerView.perform(actionOnItemAtPosition<ViewHolder>(2, click()))
+
+        val recyclerView2 = onView(
+            allOf(
+                withId(R.id.recycler_view),
+                withParent(withParent(withId(R.id.nav_host_fragment))),
+                isDisplayed()
+            )
+        )
+        recyclerView2.check(matches(isDisplayed()))
+    }
+
+    private fun childAtPosition(
+        parentMatcher: Matcher<View>, position: Int
+    ): Matcher<View> {
+
+        return object : TypeSafeMatcher<View>() {
+            override fun describeTo(description: Description) {
+                description.appendText("Child at position $position in parent ")
+                parentMatcher.describeTo(description)
+            }
+
+            public override fun matchesSafely(view: View): Boolean {
+                val parent = view.parent
+                return parent is ViewGroup && parentMatcher.matches(parent)
+                        && view == parent.getChildAt(position)
+            }
+        }
+    }
+}
